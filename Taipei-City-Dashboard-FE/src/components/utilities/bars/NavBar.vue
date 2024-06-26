a2d026434a2769840302a29b93bf8175c0fa1ab6
ABB00717 <eric94528@gmail.com>
2024-06-26 20:58:47 +0800

    Add: Now you could switch between light theme and dark theme

--- Taipei-City-Dashboard-FE/src/components/utilities/bars/NavBar.vue
+++ Taipei-City-Dashboard-FE/src/components/utilities/bars/NavBar.vue
@@ -74,13 +74,19 @@ const linkQuery = computed(() => {
     </div>
     <div class="navbar-user">
       <button
-        v-if="!(authStore.isMobileDevice && authStore.isNarrowDevice)"
+        v-if="!(authStore.isMobileDevice && authStore.isNarrowDevice) && !authStore.token"
         class="hide-if-mobile"
         @click="toggle"
       >
         <span>{{
           isFullscreen ? "fullscreen_exit" : "fullscreen"
         }}</span>
+      </button>
+      <button 
+        v-if="authStore.token" 
+        @click="authStore.toggleMode"
+      >
+        <span>{{ authStore.user.mode === 'light' ? 'dark_mode' : 'light_mode' }}</span>
       </button>
       <div class="navbar-user-info">
         <button><span>info</span></button>
